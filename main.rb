89946f71413012d0f218236f034e888c389cbfd8
Riley Mione <rmione33@gmail.com>
2020-07-25 15:47:41 -0400

    Add get_data function going to refactor soon

--- main.rb
+++ main.rb
@@ -1,6 +1,13 @@
 require 'Anki2'
+require 'csv'
 
 # It's going to be sort of a command line tool for now... so just normal print statements will do for now. 
+def get_data(csv_path) 
+    # Gets table of data to use 
+    return CSV.parse(File.read(csv_path), headers: true)
+end
+# Using CSV library 
+
 
 puts 'Enter the name of your new Anki deck: '
 deck_name = gets.chop
@@ -15,5 +22,9 @@ puts path
     name: deck_name,
     output_path: path
 })
+
+# I'm going to need to make a function which grabs the question and answer from a .csv file. 
 @anki.add_card('<span class="kanji">時</span>', 'Big')
 @anki.save
+
+puts get_data('.\data\Kanji Radicals Reference - Kanji Radicals.csv')
