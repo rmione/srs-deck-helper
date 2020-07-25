4ab7ab9720033d0e8d58bfe43e31bd5dd6fe67d2
Riley Mione <rmione33@gmail.com>
2020-07-25 13:32:14 -0400

    Commenting

--- main.rb
+++ main.rb
@@ -1,12 +1,15 @@
 require 'Anki2'
 
+# It's going to be sort of a command line tool for now... so just normal print statements will do for now. 
+
 puts 'Enter the name of your new Anki deck: '
 deck_name = gets.chop
 
+# Real basic string formatting here. 
+# going to make a YAML file that gets your configuration i.e where you want to save it, some of the config like font size etc
 path = "../generated_decks/#{deck_name}.apkg"
-sprintf(path)
-
 puts path
+
 @anki = Anki2.new({
     css: '.kanji { font-size: 88px; }',
     name: deck_name,
