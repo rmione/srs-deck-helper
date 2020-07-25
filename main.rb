c0dbe414c165257ecb8f28e4b1c51dd64daf1230
Riley Mione <rmione33@gmail.com>
2020-07-25 17:32:23 -0400

    Now works, using the CSV library seems to be a bit slow. It definitely could be my code so I will further refactor it

--- main.rb
+++ main.rb
@@ -1,33 +1,38 @@
 require 'Anki2'
 require 'csv'
 
+class Deck
+    def initialize(deck_name, dump_path)
+        @name = deck_name
+        @dump_path = dump_path
+        @deck = Anki2.new({
+            css: '.kanji { font-size: 88px; }',
+            name: @name,
+            output_path: @dump_path
+        })
+    end
+
+
+    def new_card(kanji, meaning)
+        @deck.add_card(('<span class="kanji">'+ kanji.force_encoding('utf-8') +'</span>'), meaning)
+    end
+
+    def save_deck
+        @deck.save
+    end
+    
+end
 # It's going to be sort of a command line tool for now... so just normal print statements will do for now. 
 def get_data(csv_path) 
     # Gets table of data to use 
     return CSV.parse(File.read(csv_path), encoding: 'IBM437:utf-8', headers: true)
 end
-# Using CSV library 
 
+# maybe move this to a function as well
 
-puts 'Enter the name of your new Anki deck: '
-deck_name = gets.chop
-
-# Real basic string formatting here. 
-# going to make a YAML file that gets your configuration i.e where you want to save it, some of the config like font size etc
-path = "../generated_decks/#{deck_name}.apkg"
-puts path
-
-@anki = Anki2.new({
-    css: '.kanji { font-size: 88px; }',
-    name: deck_name,
-    output_path: path
-})
-
-# I'm going to need to make a function which grabs the question and answer from a .csv file. 
-
-
+newdeck = Deck.new("Radicals reference", '.\decks\Radicals reference.apkg') 
 data = get_data('.\data\Kanji Radicals Reference - Kanji Radicals.csv')
-puts data[0]["Kanji"].encoding
-
-@anki.add_card(('<span class="kanji">'+ data[0]["Kanji"].force_encoding('utf-8') +'</span>'), data[0]["Meaning"])
-@anki.save
+for row in data
+    newdeck.new_card(row['Kanji'], row['Meaning']) 
+end
+newdeck.save_deck
