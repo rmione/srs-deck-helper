a48147df8f660953d2ec2ac492d1f0ed2409009b
Riley Mione <rmione33@gmail.com>
2020-07-26 22:30:47 -0400

    Refactoring and minor changes, gotta really fix this

--- main.rb
+++ main.rb
@@ -2,19 +2,32 @@ require 'Anki2'
 require 'csv'
 
 class Deck
-    def initialize(deck_name, dump_path)
+    def initialize(deck_name, dump_path, headers_array)
         @name = deck_name
+        @headers = headers_array.drop(1) # This omits the first header which I assume is "Kanji"
         @dump_path = dump_path
         @deck = Anki2.new({
             css: '.kanji { font-size: 88px; }',
             name: @name,
             output_path: @dump_path
         })
+        # Not quite sure if this will work or not 
+        for header in @headers
+            @base = base + header + " %s" +"\n" # header and newline 
+        end
+
     end
 
 
-    def new_card(kanji, meaning)
-        @deck.add_card(('<span class="kanji">'+ kanji.force_encoding('utf-8') +'</span>'), meaning)
+    def new_card(data)
+        kanji = data['Kanji'] # For now let's keep it like this, I am only studying Japanese for now and for sure it will be Kanji!         
+        temp = @base
+        for col in row
+            # Repeatedly fills out all the %s with all of the different columns! 
+            temp = temp % col 
+        end 
+            
+        @deck.add_card(('<span class="kanji">'+ kanji.force_encoding('utf-8') +'</span>'), 'Meaning: '+meaning + '\nRadicals: '+ radicals.force_encoding('utf-8'))
     end
 
     def save_deck
@@ -31,10 +44,12 @@ end
 # maybe move this to a function as well
 
 t1 = Time.now
-newdeck = Deck.new("Radicals reference", '.\decks\Radicals reference.apkg') 
+
 data = get_data('.\data\Kanji Radicals Reference - Kanji Radicals.csv')
+headers= data.headers # Gets the headers
+newdeck = Deck.new("Radicals reference", '.\decks\Radicals reference.apkg', headers) 
 for row in data
-    newdeck.new_card(row['Kanji'], row['Meaning']) 
+    newdeck.new_card(row) 
 end
 newdeck.save_deck
 t2 = Time.now 
